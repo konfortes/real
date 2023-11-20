50fe5c9afffebe234b847e3166ab13a0380491bc
konfortes <konfortes@gmail.com>
2023-11-20 11:13:23 +0200

    Address dataclass improvements

--- scraping/yad2_feed_item.py
+++ scraping/yad2_feed_item.py
@@ -1,24 +1,36 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import Optional
 
 
 @dataclass
 class Address:
-    city: str = ""
-    # TODO: separate street and number
-    street_and_number: str = ""
-    neighborhood: str = ""
-    floor: str = ""
+    city: Optional[str]
+    neighborhood: Optional[str]
+    floor: Optional[str]
+    street: Optional[str] = field(init=False)
+    street_number: Optional[int] = field(init=False)
+    street_and_number: Optional[str] = field(default="")
+
+    def __post__init__(self):
+        if self.street_and_number:
+            maybe_number = self.street_and_number.split(" ")[-1]
+            if maybe_number.isnumeric():
+                self.street_number = int(maybe_number)
+                self.street = " ".join(self.street_and_number.split(" ")[:-1])
+            else:
+                self.street = self.street_and_number
+                self.street_number = None
 
 
 # pylint: disable=too-many-instance-attributes
 @dataclass
 class FeedItem:
-    address: Address
-    seller_name: str = ""
-    seller_phone_number: str = ""
-    estate_type: str = ""
-    rooms: str = ""
-    square_meter: str = ""
-    price: str = ""
-    updated_date: str = ""
-    description: str = ""
+    address: Address = field(hash=True)
+    seller_name: str = field(hash=True, default="")
+    seller_phone_number: str = field(hash=True, default="")
+    estate_type: str = field(hash=True, default="")
+    rooms: str = field(hash=True, default="")
+    square_meter: str = field(hash=True, default="")
+    price: str = field(hash=False, default="")
+    updated_date: str = field(hash=False, compare=False, default="")
+    description: str = field(hash=False, repr=False, compare=False, default="")
